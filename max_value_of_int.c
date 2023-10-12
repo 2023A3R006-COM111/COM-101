cbc9456e34b20bbd397e21f796780051818fab47
2023A3R006-COM111 <146424973+2023A3R006-COM111@users.noreply.github.com>
2023-10-12 22:38:29 +0530

    Add files via upload

---
+++ max_value_of_int.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    //checking the maximum value of 16 bit compiler
+        printf("integer values\n\n");
+            printf("%d %d %d\n",32767,32767+1,2147483647);
+                printf("\n");
+            printf("long integer values\n\n");
+        printf("%ld %ld %ld\n",32767L,32767+1L,32767L+10L);
+    return 0;
+}
