f5c690b818e1ab67fbedd6fa55cbae5ed157869c
kartik sharma <146424973+2023A3R006-COM111@users.noreply.github.com>
2023-11-03 18:16:49 +0530

    Add files via upload

---
+++ size_off.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int integerVar;
+    float floatVar;
+    char charVar;
+
+    printf("Size of int: %lu bytes\n",sizeof(int));
+    print("Size of float: %lu bytes\n",sizeof(float));
+    print("Size of char: %lu bytes\n",sizeof(char));
+
+    printf("Size of integerVar: %lu bytes\n",sizeof(integerVar));
+    printf("Size of floatVar: %lu bytes\n",sizeof(floatVar));
+    printf("Size of charVar: %lu bytes\n",sizeof(charVar));
+
+    return 0;
+
+}
